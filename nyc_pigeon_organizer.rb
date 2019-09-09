9bd6d44dae6e894da01df720170a9d67b8ffdd3d
sunny-utility-4827 <mzakzook@me.com>
2019-09-09 22:29:18 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,43 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ pigeon_list = {}
+ data.each do |keys, values|
+   values.each do |value, names|
+     names.each do |name|
+       if !pigeon_list[name]
+         pigeon_list[name] = {}
+         pigeon_list[name][keys] = []
+         pigeon_list[name][keys] << value.to_s
+       else
+        if pigeon_list[name][keys]
+          pigeon_list[name][keys] << value.to_s
+        else
+          pigeon_list[name][keys] = []
+          pigeon_list[name][keys] << value.to_s
+        end
+       end
+     end
+   end
+  end
+
+pigeon_list          
 end
+
+
+nyc_pigeon_organizer({
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+})
